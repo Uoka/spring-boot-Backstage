8addb8ce6527b9e9569811e75cd0eceb0e9ea6e1
yuanzf <yuanzf233@gmail.com>
2018-11-14 22:12:05 +0800

    build

---
+++ src/main/java/com/uoka/springbootbackstage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.uoka.springbootbackstage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootBackstageApplication.class);
+    }
+
+}
---
+++ src/main/java/com/uoka/springbootbackstage/SpringBootBackstageApplication.java
@@ -0,0 +1,12 @@
+package com.uoka.springbootbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackstageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackstageApplication.class, args);
+    }
+}
